674c64f926345c491dec941940eb9a37d03d43aa
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2024-01-19 12:14:58 +0900

    updated

---
+++ Week01/Helper.js
@@ -0,0 +1,3 @@
+function click01(){
+    alert("누군지 모르지만 인기가 많은 여자");
+}
---
+++ Week01/Helper02.js
@@ -0,0 +1,4 @@
+function click01(){
+    // alert(document.getElementById("target").textContent);
+    document.getElementById("target").textContent = "서환민";
+}
