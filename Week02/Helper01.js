bdfc4947de31496d9e8e08f20fe9434a7571d16c
Minhyuk Adam Oh <maothesolution@gmail.com>
2024-01-26 11:56:12 +0900

    updated

---
+++ Week02/Helper01.js
@@ -0,0 +1,3 @@
+function click02(){
+    document.body.innerHTML = '<img onclick="click02()" src="./img/img02.png" alt="red apple" width="500px" height="500px">';
+}
---
+++ Week02/cardGame/Helper.js
@@ -0,0 +1,3 @@
+function flip(){
+    document.getElementById("target").innerHTML = '<img src="./img/back.png" alt="red apple" width="250px" height="500px">'
+}
