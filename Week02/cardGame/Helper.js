dfb07e47fed99af9674630a2a051c3aa45d037d0
Minhyuk Adam Oh <maothesolution@gmail.com>
2024-01-30 10:22:12 +0900

    updated

--- Week02/cardGame/Helper.js
+++ Week02/cardGame/Helper.js
@@ -1,3 +1,9 @@
 function flip(){
     document.getElementById("target").innerHTML = '<img src="./img/back.png" alt="red apple" width="250px" height="500px">'
 }
+
+function helper(){
+    const d = new Date();
+    addZero(d.getSeconds());
+
+}    
