03ac67b18afb125adf4d7b1f9bb9304083881e99
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2024-02-08 14:19:19 +0900

    updated

---
+++ Week04/Helper01.js
@@ -0,0 +1,6 @@
+
+// console.log("This is the king!");
+// document.getElementById("td00").innerHTML = '<img src="./img01.png" alt="red apple" width="125px" height="250">';
+xpos = Math.floor(Math.random()*2)
+ypos = Math.floor(Math.random()*2)
+document.getElementById("td"+xpos.toString()+ypos.toString()).innerHTML = '<img src="./img02.png" alt="red apple" width="125px" height="250">'
